b866065e9a7f7cb0741ab5593f203be91f360ad9
marzanoo <tanzanowayaw@gmail.com>
2025-01-21 22:13:57 +0700

    rendering dynamic routes

--- src/pages/product/[product].tsx
+++ src/pages/product/[product].tsx
@@ -1,13 +1,61 @@
+import { fetcher } from "@/lib/swr/fetcher";
+import { ProductType } from "@/types/product.type";
+import DetailProduct from "@/views/DetailProduct";
 import { useRouter } from "next/router";
+import useSWR from "swr";
 
-const DetailProductPage = () => {
+const DetailProductPage = ({ product }: { product: ProductType }) => {
   const { query } = useRouter();
+  // const { data, error, isLoading } = useSWR(
+  //   `/api/product/${query.product}`,
+  //   fetcher
+  // );
+
   return (
     <div>
-      <h1>Detail Product</h1>
-      <p>Product : {query.product}</p>
+      {/* <DetailProduct product={isLoading ? [] : data.data} /> */}
+      <DetailProduct product={product} />
     </div>
   );
 };
+// export async function getServerSideProps({
+//   params,
+// }: {
+//   params: { product: string };
+// }) {
+//   // fetch data
+//   const res = await fetch(
+//     `http://localhost:3000/api/product/${params.product}`
+//   );
+//   const response = await res.json();
 
+//   return { props: { product: response.data } };
+// }
+
+export async function getStaticPaths() {
+  const res = await fetch("http://localhost:3000/api/product");
+  const response = await res.json();
+
+  const paths = response.data.map((product: ProductType) => ({
+    params: {
+      product: product.id,
+    },
+  }));
+
+  return { paths, fallback: false };
+}
+
+export async function getStaticProps({
+  params,
+}: {
+  params: { product: string };
+}) {
+  // fetch data
+  const res = await fetch(
+    "http://localhost:3000/api/product/" + params.product
+  );
+  const response = await res.json();
+
+  return { props: { product: response.data } };
+}
 export default DetailProductPage;
