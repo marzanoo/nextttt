e9eca68d462c99d328ffec01d354be78c5454405
marzanoo <tanzanowayaw@gmail.com>
2025-01-24 14:46:35 +0700

    setup next auth

--- src/components/layouts/Navbar/index.tsx
+++ src/components/layouts/Navbar/index.tsx
@@ -1,9 +1,23 @@
+import { signIn, signOut, useSession } from "next-auth/react";
 import stylesNavbar from "./Navbar.module.css";
 
 const Navbar = () => {
+  const { data }: any = useSession();
   return (
     <div className={stylesNavbar.navbar}>
-      <div>Navbar</div>
+      <div className="big">Navbar</div>
+      <div>
+        {data && data.user.fullname}
+        {data ? (
+          <button className={stylesNavbar.button} onClick={() => signOut()}>
+            Sign Out
+          </button>
+        ) : (
+          <button className={stylesNavbar.button} onClick={() => signIn()}>
+            Sign In
+          </button>
+        )}
+      </div>
     </div>
   );
 };
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,10 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import withAuth from "./middlewares/withAuth";
 
-export function middleware(request: NextRequest) {
-  const isLogin = true;
-  if (isLogin) {
-    return NextResponse.next();
-  } else {
-    return NextResponse.redirect(new URL("/auth/login", request.url));
-  }
+export function mainMiddleware(request: NextRequest) {
+  const res = NextResponse.next();
+  return res;
 }
 
-export const config = {
-  matcher: ["/product", "/about"],
-};
+export default withAuth(mainMiddleware, ["/profile"]);
